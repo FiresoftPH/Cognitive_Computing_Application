dd8809b10fa8db32fa6bdf6113cf642208463681
FiresoftGH <most.chuti@gmail.com>
2023-01-18 15:04:11 +0700

    UWU

--- Week_3/lab.py
+++ Week_3/lab.py
@@ -16,13 +16,13 @@ x2, y2, w2, h2 = 386, 290, 386, 290
 alpha = 0.7
   
 # A filled rectangle
-cv2.rectangle(overlay, (x, y), (x + w, y + h), (255, 0, 0), -1)
+cv2.rectangle(overlay, (x, y), (x + w, y + h), (150, 0, 0), -1)
 cv2.rectangle(overlay, (x1, y1), (x1 + w1, y1 + h1), (0, 120, 0), -1)
-cv2.rectangle(overlay, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 100), -1)
+cv2.rectangle(overlay, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 80), -1)
   
 image_new = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
 
-cv2.imshow("filter", image_new)
+cv2.imshow("Pattarapark Chutisamoot", image_new)
 
 cv2.waitKey(0)
   
